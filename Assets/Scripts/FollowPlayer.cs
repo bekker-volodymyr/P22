5bc40d7a669fd3b6dbf03c677b936e044d40e3ce
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-11-01 16:14:16 +0200

    init commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private Vector3 _offset = new Vector3(0f, 6f, -15f);
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = _player.transform.position + _offset;
+    }
+}
