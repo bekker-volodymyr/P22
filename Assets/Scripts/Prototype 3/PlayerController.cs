3fd52c441ecd17355cece4654cd1a88da375c0ea
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-11-07 19:36:20 +0200

    vfx sound import prototype 4

--- Assets/Scripts/Prototype 3/PlayerController.cs
+++ Assets/Scripts/Prototype 3/PlayerController.cs
@@ -7,6 +7,20 @@ namespace Prototype3
     {
         private Rigidbody _rb;
         private Animator _animator;
+        private AudioSource _audioSource;
+
+        [SerializeField]
+        private AudioSource _musicSource;
+
+        [SerializeField]
+        private AudioClip _crashSound;
+        [SerializeField]
+        private AudioClip _jumpSound;
+
+        [SerializeField]
+        private ParticleSystem _explosionEffect;
+        [SerializeField]
+        private ParticleSystem _dirtEffect;
 
         [SerializeField]
         private float _jumpForce = 250f;
@@ -23,6 +37,7 @@ namespace Prototype3
             Physics.gravity *= _gravityModifier;
 
             _animator = GetComponent<Animator>();
+            _audioSource = GetComponent<AudioSource>();
 
             _jumpAction = InputSystem.actions.FindAction("Jump");
             _jumpAction.performed += Jump;
@@ -32,6 +47,8 @@ namespace Prototype3
         {
             if (_isGrounded && !GameManager.IsGameOver)
             {
+                _audioSource.PlayOneShot(_jumpSound, 1.0f);
+                _dirtEffect.Stop();
                 _animator.SetTrigger("Jump_trig");
                 _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
                 _isGrounded = false;
@@ -43,9 +60,18 @@ namespace Prototype3
             if (collision.gameObject.CompareTag("Ground"))
             {
                 _isGrounded = true;
+                if (!GameManager.IsGameOver)
+                {
+                    _dirtEffect.Play();
+                }
             }
             else if (collision.gameObject.CompareTag("Obstacle"))
             {
+                Destroy(_musicSource);
+                //_musicSource.volume = 0.1f;
+                _audioSource.PlayOneShot(_crashSound, 1.0f);
+                _dirtEffect.Stop();
+                _explosionEffect.Play();
                 _animator.SetBool("Death_b", true);
                 _animator.SetInteger("DeathType_int", 1);
                 GameManager.IsGameOver = true;
